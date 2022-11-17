9c922775f989f50c6c78764a68de01e3dd00e13e
Helcio <helcio@carmo.in>
2022-11-17 20:29:08 -0300

    feat: Setup userStore

---
+++ src/store/user.ts
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface User {
+    name: string
+    email: string
+    password: string
+}
+
+interface UserState {
+    users: User[]
+    currentUser: User | null
+}
+
+const initialState: UserState = {
+    users: [],
+    currentUser: null
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        createUser: (state, action: PayloadAction<User>) => {
+            state.users.push(action.payload)
+        },
+
+        login: (state, action: PayloadAction<Omit<User, 'name'>>) => {
+            const user = state.users.filter((user) => user.email === action.payload.email)[0]
+            state.currentUser = user.password === action.payload.password ? user : null
+        },
+
+        logout: (state) => { state.currentUser = null }
+    }
+})
+
+export const { createUser, login, logout } = userSlice.actions
+
+export default userSlice.reducer
